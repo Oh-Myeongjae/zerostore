8384988bbaa5940ac97a5677f0080e87e8ccb8be
Oh-Myeongjae <omjcws@gmail.com>
2024-12-27 23:49:09 +0900

    feat: 프로젝트 초기세팅

---
+++ src/main/java/com/zerobase/zerostore/ZerostoreApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.zerostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerostoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZerostoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zerobase/zerostore/ZerostoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.zerostore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerostoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
