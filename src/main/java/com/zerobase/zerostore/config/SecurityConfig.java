1a988f93d1781ce27034eb02fd8374d46786726d
Oh-Myeongjae <omjcws@gmail.com>
2024-12-28 15:43:48 +0900

    removed cached

---
+++ src/main/java/com/zerobase/zerostore/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.zerobase.zerostore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+//@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/**", "/login/**", "/signup/**").permitAll() // 인증 없이 접근 허용
+                        .anyRequest().authenticated() // 그 외 모든 요청은 인증 필요
+                );
+        return http.build();
+    }
+}
+
